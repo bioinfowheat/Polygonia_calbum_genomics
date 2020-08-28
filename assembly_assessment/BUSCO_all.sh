7e2b29943382f3a4d2a5084874ce1c5746b2cfd1
Chris Wheat <chriswheat@zoologi195.zoologi.su.se>
2020-08-28 11:29:33 +0200

    update

--- assembly_assessment/BUSCO_all.sh
+++ assembly_assessment/BUSCO_all.sh
@@ -122,5 +122,5 @@ outfile=Pc_album.mpss.cln.msk.haploidA_v_lep_odb10
 # parameters
 export BUSCO_CONFIG_FILE=/data/programs/busco-4.1.2/config/config.ini
 python3 /data/programs/busco-4.1.2/bin/busco -i $genome -l $library -m genome -o $outfile -c 30
-
+# then assess
 more Pc_album.mpss.cln.msk.haploidA_v_lep_odb10/short_summary.*
