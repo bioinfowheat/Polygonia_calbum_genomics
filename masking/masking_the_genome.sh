e793046f17922ea26f36d8cc4cb63120f1ea0f93
Chris Wheat <chriswheat@zoologi195.zoologi.su.se>
2020-09-02 17:34:18 +0200

    udpate

--- masking/masking_the_genome.sh
+++ masking/masking_the_genome.sh
@@ -2,16 +2,42 @@
 
 mkdir masking
 cd masking/
-mkdir genome
-cd genome
-ln -s /mnt/griffin/chrwhe/Pc_album/10x/Scaffolds_pass1.fa .
-cd ..
-mkdir masked_out
-mkdir repeats_out
 
-# The following command runs Red with the defaults and generates the masked sequences and the locations of the repeats found in the whole genome.
-#
-# Red -gnm genome_directory -msk output_directory -rpt output_directory
+# get your genome
+ln -s ../../Pieris_rapae_fullAsm.fasta .
+
+# define a path for the Red software
+export PATH=/data/programs/RED/redUnix64/:$PATH
+# run the script with your genome and output header named (this output header will be used for names of the new files generated).
+python /mnt/griffin/chrwhe/software/redmask/redmask.py -i Pieris_rapae_fullAsm.fasta -o Pieris_rapae_fullAsm > results.log
+
+# see what you got
+more results.log
+[Sep 02 04:48 PM] Running Python v2.7.16
+[Sep 02 04:48 PM] Loading assembly:
+    Contigs: 2,772
+    Length:  323,179,347 bp
+    nN50:    11,535,178 bp
+[Sep 02 04:48 PM] Splitting genome assembly into training set (contigs > 1000 bp)
+[Sep 02 04:48 PM] Finding repeats with Red (REpeat Detector)
+[Sep 02 04:58 PM] Collecting results from Red
+[Sep 02 04:58 PM] Summarizing results and converting to BED format
+
+Masked genome: /mnt/griffin/chrwhe/Pieris_rapae_v1/genome_fullASM/masking/Pieris_rapae_fullAsm.softmasked.fa
+Repeat BED file: /mnt/griffin/chrwhe/Pieris_rapae_v1/genome_fullASM/masking/Pieris_rapae_fullAsm.repeats.bed
+Repeat FASTA file: /mnt/griffin/chrwhe/Pieris_rapae_v1/genome_fullASM/masking/Pieris_rapae_fullAsm.repeats.fasta
+num scaffolds: 2,772
+assembly size: 323,179,347 bp
+masked repeats: 118,710,751 bp (36.73%)
 
 
-/data/programs/RED/redUnix64/Red -gnm genome -msk masked_out -rpt repeats_out -frm 2
+
+#############
+# install issues
+
+# install needed package
+pip install natsort
+# export path to RED
+export PATH=/data/programs/RED/redUnix64/:$PATH
+# run the script.
+python /mnt/griffin/chrwhe/software/redmask/redmask.py -i Pieris_rapae_fullAsm.fasta -o output > results.log
