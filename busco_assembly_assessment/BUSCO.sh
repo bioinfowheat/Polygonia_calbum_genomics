6ff400bf22c0825712e9b86b4dd83a5f8e99036e
Maria Celorio <maria.celorio@zoologi.su.se>
2021-03-01 15:41:42 +0100

    Update BUSCO.sh

--- busco_assembly_assessment/BUSCO.sh
+++ busco_assembly_assessment/BUSCO.sh
@@ -4,31 +4,30 @@
 # plotting
 https://busco.ezlab.org/busco_userguide.html#companion-scripts
 
-# note that you can generate some simple plots that might be nice for comparing among different assemblies
-# during the process of polishing, etc.
-
-
-genome=Pcalbum_chromosome.fa
-library=insecta_odb10
-outfile=Pcalbum_chromosome_v_insecta_odb10
+genome=Pcalbum_chromosome_and_unplaced.fa
+library=lepidoptera_odb10
+outfile=Pcalbum_chromosome_and_unplaced_v_lep_odb10
 
 # parameters
 export BUSCO_CONFIG_FILE=/data/programs/busco-4.1.2/config/config.ini
 python3 /data/programs/busco-4.1.2/bin/busco -i $genome -l $library -m genome -o $outfile -c 30
-
-more short_summary.specific.insecta_odb10.Pcalbum_chromosome_v_insecta_odb10.txt
+# then assess
+head -30 Pcalbum_chromosome_and_unplaced_v_lep_odb10/short_summary.specific.lepidoptera_odb10.Pcalbum_chromosome_and_unplaced_v_lep_odb10.txt
 # BUSCO version is: 5.beta
-# The lineage dataset is: insecta_odb10 (Creation date: 2020-08-05, number of species: 75, number of BUSCOs: 1367)
-# Summarized benchmarking in BUSCO notation for file /mnt/griffin/chrwhe/Polygonia_calbum/chromosome_level/busco/Pcalbum_chromoso
-me.fa
+# The lineage dataset is: lepidoptera_odb10 (Creation date: 2020-08-05, number of species: 16, number of BUSCOs: 5286)
+# Summarized benchmarking in BUSCO notation for file /mnt/griffin/Polygonia_genome/genomes/Pc_album.mpss.cln.msk.haploidA.fa
 # BUSCO was run in mode: genome
 
-        ***** Results: *****
 
-        C:80.8%[S:80.7%,D:0.1%],F:3.9%,M:15.3%,n:1367
-        1105    Complete BUSCOs (C)
-        1103    Complete and single-copy BUSCOs (S)
-        2       Complete and duplicated BUSCOs (D)
-        53      Fragmented BUSCOs (F)
-        209     Missing BUSCOs (M)
-        1367    Total BUSCO groups searched
+--------------------------------------------------
+        |Results from dataset lepidoptera_odb10           |
+        --------------------------------------------------
+        |C:90.4%[S:90.1%,D:0.3%],F:4.7%,M:4.9%,n:5286     |
+        |4780   Complete BUSCOs (C)                       |
+        |4763   Complete and single-copy BUSCOs (S)       |
+        |17     Complete and duplicated BUSCOs (D)        |
+        |247    Fragmented BUSCOs (F)                     |
+        |259    Missing BUSCOs (M)                        |
+        |5286   Total BUSCO groups searched               |
+        --------------------------------------------------
+INFO:   BUSCO analysis done. Total running time: 622 seconds
