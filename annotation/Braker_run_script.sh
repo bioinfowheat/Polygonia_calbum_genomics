163ca9df710fa6cca44f1a76e73cc117e090252e
Maria Celorio <maria.celorio@zoologi.su.se>
2021-03-01 15:33:02 +0100

    Update Braker_run_script.sh

--- annotation/Braker_run_script.sh
+++ annotation/Braker_run_script.sh
@@ -1,6 +1,6 @@
 #annotation with only protein dataset
 
-genome=softmasked.fa
+genome=Pcalbum_chromosome_and_unplaced.fa
 proteins=odb10_arthropoda_proteins.fa
 
 ###########
@@ -33,12 +33,49 @@ export MAKEHUB_PATH=/data/programs/MakeHub/
 
 braker.pl --genome=$genome --prot_seq=$proteins --softmasking --cores=30
 
-# annotation using protein and bam files from RNAseq
+
+#Annotation using only bam files from RNAseq
+
+ls *aln.sorted.bam > bams
+
+bamfiles=$(tr '\n' ',' < bams)
+
+genome=Pcalbum_chromosome_and_unplaced.fa
+
+species=Pca
+
+#the key
+cp /data/programs/scripts/gm_key_64 gm_key
+cp gm_key ~/.gm_key
+
+
+your_working_dir=$(pwd)
+cp -r /data/programs/Augustus_v3.3.3/config/ $your_working_dir/augustus_config
+export AUGUSTUS_CONFIG_PATH=$your_working_dir/augustus_config
+chmod -R 777 $your_working_dir/augustus_config
+
+#the other tools
+export PATH=/data/programs/BRAKER2_v2.1.5/scripts/:$PATH
+export AUGUSTUS_BIN_PATH=/data/programs/Augustus_v3.3.3/bin
+export AUGUSTUS_SCRIPTS_PATH=/data/programs/Augustus_v3.3.3/scripts
+export DIAMOND_PATH=/data/programs/diamond_v0.9.24/
+export GENEMARK_PATH=/data/programs/gmes_linux_64.4.61_lic/
+export BAMTOOLS_PATH=/data/programs/bamtools-2.5.1/bin/
+export PROTHINT_PATH=/data/programs/ProtHint/bin/
+export ALIGNMENT_TOOL_PATH=/data/programs/gth-1.7.0-Linux_x86_64-64bit/bin
+export SAMTOOLS_PATH=/data/programs/samtools-1.10/
+export MAKEHUB_PATH=/data/programs/MakeHub/
+
+
+
+braker.pl --genome=$genome --bam=$bamfiles --softmasking
+
+#Annotation using protein and bam files from RNAseq
 # ls *bam | sed 's/.bam/.bam,/g' | tr "\n" " "| sed 's/ //g' > bam_files.csv
 
 
 ln -s /Pca_genome/annotation/RNA-seq/*fastq.gz .
-ln -s /assemblies/Pca_genome.fasta .
+ln -s /assemblies/Pcalbum_chromosome_and_unplaced.fa .
 
 
 # Align RNA seq
@@ -65,11 +102,11 @@ done
 
 #softmask genome using RED
 
-bash redmask.sh Pca_genome.fasta Pca_genome.softmasked
+bash redmask.sh Pca_genome.fasta Pcalbum_chromosome_and_unplaced.fa
 
 # Run braker2
 ls *aln.sorted.bam > bams
-genome=Pca_genome.softmasked.fa
+genome=Pcalbum_chromosome_and_unplaced.fa
 bamfiles=$(tr '\n' ',' < bams)
 proteins=odb10_arthropoda_proteins.fa
 species=Pca
