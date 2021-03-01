5dfdf02beaa5b1216f2219475bdd070fc2955e21
Maria Celorio <maria.celorio@zoologi.su.se>
2021-03-01 15:29:01 +0100

    Update Braker_run_script.sh

--- annotation/Braker_run_script.sh
+++ annotation/Braker_run_script.sh
@@ -37,3 +37,55 @@ braker.pl --genome=$genome --prot_seq=$proteins --softmasking --cores=30
 # ls *bam | sed 's/.bam/.bam,/g' | tr "\n" " "| sed 's/ //g' > bam_files.csv
 
 
+ln -s /Pca_genome/annotation/RNA-seq/*fastq.gz .
+ln -s /assemblies/Pca_genome.fasta .
+
+
+# Align RNA seq
+# index the genome
+
+hisat2=/data/programs/hisat2-master/
+
+$hisat2/hisat2-build $reference ${genome%.fasta}_Hi2index
+
+for sample in `ls ./*_1.fastq.gz `
+do base=$(basename $sample "_1.fastq.gz")
+$hisat2/hisat2 -p 40 -x ${reference%.fasta}_Hi2index -S ${base}.sam -q -1 ${base}_1.fastq.gz -2 ${base}_2.fastq.gz
+done
+
+
+# convert to bams and filter out unmapped reads
+for i in *sam
+do samtools view -bS -F 4 $i > ${i%.sam}.aln.bam
+done
+
+for i in *aln.bam
+do sambamba sort -t 32 $i
+done
+
+#softmask genome using RED
+
+bash redmask.sh Pca_genome.fasta Pca_genome.softmasked
+
+# Run braker2
+ls *aln.sorted.bam > bams
+genome=Pca_genome.softmasked.fa
+bamfiles=$(tr '\n' ',' < bams)
+proteins=odb10_arthropoda_proteins.fa
+species=Pca
+export PATH=/data/programs/augustus-3.3.1/bin/:/cerberus/projects/kaltun/software/BRAKER/scripts/:/cerberus/projects/kaltun/software/BRAKER/:$PATH
+export GENEMARK_PATH=/data/programs/gm_et_linux_64/gmes_petap
+export AUGUSTUS_CONFIG_PATH=/data/programs/Augustus-3.3.1/config
+export AUGUSTUS_SCRIPTS_PATH=/data/programs/Augustus-3.3.1/scripts
+export BAMTOOLS_PATH=/data/programs/bamtools-2.5.1/bin/
+export BLAST_PATH=/data/programs/ncbi-blast-2.5.0+/bin
+export SAMTOOLS_PATH=/data/programs/samtools-1.10/
+# setting the alignment tool
+export ALIGNMENT_TOOL_PATH=/data/programs/gth-1.7.0-Linux_x86_64-64bit/bin
+# input
+# input
+braker.pl --species=$species --genome=$genome \
+--bam=bamfiles --prot_seq=$proteins \
+--prg=gth --gth2traingenes --cores 40 \
+--softmasking
+
